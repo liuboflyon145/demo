84cdfc79c8b9dbffe0b6476ae564cf5fc7f1ddea
liuboflyon145 <lobal9145>
2017-03-02 21:51:32 +0800

    add

---
+++ dubbo-server/src/main/java/com/example/provider/impl/LoginServiceImpl.java
@@ -0,0 +1,30 @@
+package com.example.provider.impl;
+
+import com.alibaba.dubbo.config.annotation.Service;
+import com.example.common.MessageEnum;
+import com.example.common.ResultFactory;
+import com.example.common.ResultObj;
+import com.example.domain.User;
+import com.example.provider.ILoginService;
+import com.example.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+
+/**
+ * Created by liubo on 2017/3/1.
+ */
+@Service(version = "1.0.0")
+public class LoginServiceImpl implements ILoginService {
+
+    @Autowired
+    UserService userService;
+    @Override
+    public ResultObj login(String phone, String password) {
+        User user = userService.getUser(phone);
+        if (user==null)
+            return ResultFactory.getResultObj(null,MessageEnum.USERNOTFOUND);
+        if (password.length()>0&&password.equals(user.getPassword())){
+            return ResultFactory.getResultObj(user, MessageEnum.SUCCESS);
+        }
+        return ResultFactory.getResultObj(user,MessageEnum.PASSWORDERROR);
+    }
+}
